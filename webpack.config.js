6cbf5c9c232a30ab73cc20857fd8032c41dacda6
Kszalank <szalankiewiczkrzysiek@gmail.com>
2022-07-11 19:34:09 +0200

    development changed to production

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-const isProduction = process.env.NODE_ENV == "development";
+const isProduction = process.env.NODE_ENV == "production";
 
 const config = {
   entry: "./src/index.js",
