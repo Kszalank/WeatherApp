9d7f981c44da17df7076991808b7fdcb942b85b8
Kszalank <szalankiewiczkrzysiek@gmail.com>
2022-07-13 17:58:42 +0200

    webpack config changed

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-const isProduction = process.env.NODE_ENV == "development";
+const isProduction = process.env.NODE_ENV == "production";
 
 const config = {
   entry: "./src/index.js",
@@ -36,7 +36,7 @@ const config = {
       },
       {
         test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-        type: "asset/resource",
+        type: "asset",
       },
       // Add your rules for custom modules here
       // Learn more about loaders from https://webpack.js.org/loaders/
