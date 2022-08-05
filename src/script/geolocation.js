701fd2e19bdbada10b7421732f6886aa62165b01
Kszalank <szalankiewiczkrzysiek@gmail.com>
2022-08-05 20:43:15 +0200

    geolocation code changed

--- src/script/geolocation.js
+++ src/script/geolocation.js
@@ -1,50 +1,17 @@
-import { weather } from "./forecast";
+import { fetchWeather } from "./forecast";
 
-let geocode = {
-  reverseGeocode: function (latitude, longitude) {
-    var api_key = "760378137c2a4f75b614eece657dbd9a";
-    var api_url = "https://api.opencagedata.com/geocode/v1/json";
+function searchFromGeolocation({ latitude, longitude }) {
+  fetchWeather(`&lat=${latitude}&lon=${longitude}&units=metric`);
+}
 
-    var request_url =
-      api_url +
-      "?" +
-      "key=" +
-      api_key +
-      "&q=" +
-      encodeURIComponent(latitude + "," + longitude) +
-      "&pretty=1" +
-      "&no_annotations=1";
-    var request = new XMLHttpRequest();
-    request.open("GET", request_url, true);
+function success(data) {
+  searchFromGeolocation(data.coords);
+}
 
-    request.onload = function () {
-      if (request.status === 200) {
-        var data = JSON.parse(request.responseText);
-        weather.fetchWeather(data.results[0].components.city);
-      } else if (request.status <= 500) {
-        console.log("unable to geocode! Response code: " + request.status);
-        var data = JSON.parse(request.responseText);
-        console.log("error msg: " + data.status.message);
-      } else {
-        console.log("server error");
-      }
-    };
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(success, console.error);
+  }
+}
 
-    request.onerror = function () {
-      console.log("unable to connect to server");
-    };
-
-    request.send();
-  },
-  getLocation: function () {
-    function success(data) {
-      geocode.reverseGeocode(data.coords.latitude, data.coords.longitude);
-    }
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(success, console.error);
-    } else {
-      weather.fetchWeather("");
-    }
-  },
-};
-geocode.getLocation();
+getLocation();
