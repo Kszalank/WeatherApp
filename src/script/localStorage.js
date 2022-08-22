0f425432895ab07c4ecb34d1ce67d2ce2b02a0c3
Kszalank <szalankiewiczkrzysiek@gmail.com>
2022-08-22 17:40:13 +0200

    if statement changed in local storage

--- src/script/localStorage.js
+++ src/script/localStorage.js
@@ -7,8 +7,7 @@ function storeCity(cityName) {
 
 function fail() {
   const city = localStorage.getItem("city");
-  if (city === 0) {
-  } else {
+  if (city !== 0) {
     fetchWeather(`&cnt=72&units=metric&q=${city}`);
   }
 }
