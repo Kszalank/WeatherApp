e8ea06a3395793616ac6d1a988db32f54034d4ca
Kszalank <szalankiewiczkrzysiek@gmail.com>
2023-05-15 10:25:10 +0200

    background changed

--- src/script/backgroundUpdate.js
+++ src/script/backgroundUpdate.js
@@ -4,7 +4,7 @@ const backgroundColor = document.querySelector(".body");
 function updateBackground(currentTime, sunrise, sunset) {
   if (currentTime > sunrise && currentTime < sunset) {
     backgroundColor.style.background =
-      "linear-gradient(180deg, #62c2c8 0%, rgba(115, 238, 246, 0.16) 99.99%, rgba(118, 247, 255, 0) 100%)";
+      "linear-gradient(180deg, #62c2c8 0%, rgba(115, 238, 246, 0.16) 130%, rgba(118, 247, 255, 0) 150%)";
   } else {
     backgroundColor.style.background = "linear-gradient(180deg, #4B5C99 0%, rgba(98, 194, 200, 0) 100%)";
   }
